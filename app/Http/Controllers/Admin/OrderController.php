9519483f8a755d018a75ae7ce94fb476051c21d4
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2023-12-04 02:20:37 +0300

    Update OrderController.php

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -257,7 +257,7 @@ class OrderController extends Controller
      */
     public function show($id)
     {
-        $data['order'] = Order::with(['road.driver','customer','files'])->findOrFail($id);
+        $data['order'] =  Order::with(['files','customer','items','devices','questions','payments'])->findOrFail($id);
 
         $data['title'] = trans('Order Details');
 
